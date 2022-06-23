e557cd9b33072e655620d63d6a667a8a5234282d
George Smpokos <99085568+georgesmpokos@users.noreply.github.com>
2022-06-23 21:56:31 +0300

    Update covid_new.py

--- covid_new.py
+++ covid_new.py
@@ -1,13 +1,16 @@
+# ------------------------------------------------------ import libraries -------------------------------------------------
 import numpy as np
 import pandas as pd
 from scipy import signal
 
 #from datetime import time
 #from datetime import date
-
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 #import plotly.figure_factory as ff
-
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 #from math import sqrt
 #from sklearn import preprocessing
 
@@ -36,19 +39,19 @@ k=2209/290
 t=(29/47)
 dist=gamma(k,scale=t)
 
-def Rt(df,ix0=-1,smooth="10D",k=2209/290,t=29/47):
+def Rt(df,ix0=-1,smooth="10D",k=2209/841,t=841/470):
     Irm=df.rolling(smooth).mean().new_cases
     s=0
     dist=gamma(k,scale=t)
-    for ix in range(0,15):    
+    for ix in range(0,20):    
         s+=dist.pdf(ix)*Irm[ix0-ix]
         #print(ix,ix0-ix,Irm[ix0-ix],Irm[ix0]/s) 
     return Irm[ix0]/s
-
+  
 df["Rt"]=np.nan
 for i in range(len(df)):
     df["Rt"].iloc[i]=Rt(df,i)
-
+    
 
 # ------------------------------------------------------ streamlit page layout -------------------------------------------------
 # Use the full page instead of a narrow central column
@@ -56,6 +59,7 @@ st.set_page_config(layout="wide")
 
 # display title
 st.title('Greece covid analytics Dashboard')
+st.subheader('Daily Report')
 
 
 
@@ -74,24 +78,24 @@ value_labels={"New Cases":'new_cases',"Rt":"Rt","New Tests":'new_tests',"New Pos
 
 
 Rows={"Cases and Testing":["New Cases","Rt","New Tests","New Positive Tests"],
-     "Hospitalization and Mortallity":["New Hospitalizations","NewCritical","NewDeaths","ICU out"],
+     "Hospitalization and Mortallity":["New Hospitalizations","New Critical","New Deaths","ICU out"],
       "Vaccinations":["Total Vaccinations",'','','']
      }
 
 
 for Row in Rows: #Row is every key in dictionary Rows 
-    # loop 0: Row="Cases and Testing" , Rows[Row]=["New Cases","Rt","NewTests","New Positive Tests"]
+    # loop 0: Row="Cases and Testing" , Rows[Row]=["New Cases","Rt","New Tests","New Positive Tests"]
     cols = st.columns(tuple([0.5]+[1]*len(Rows[Row]))) #tuple([0.5]+[1]*len(Rows[Row]))= (0.5,1,1,1,1)
-
+    
     with cols[0]:
         st.markdown(Row)
-
+        
     #zip
     #for i,j,k in zip(['a','b','c'],[1,2,3],[7,8,9]):
     #i='a',j=1,k=7
     #'b',2,8
     #'c',3,9
-
+    
     for ci,label in zip(cols[1:],Rows[Row]):
         #loop 0: ci = cols[1], label="New Cases"
         if label != '':
@@ -100,14 +104,14 @@ for Row in Rows: #Row is every key in dictionary Rows
                 val=df.iloc[-1][info]
                 dif=df.iloc[-1][info]-df.iloc[-2][info]
             else: 
-
+                
                 #it is only about Hospitalizations, which is around one day before
-               notna=df[~pd.isna(df[info])] #temporary dataframe (with random name notna), without nan values
+                notna=df[~pd.isna(df[info])] #temporary dataframe (with random name notna), without nan values
                 #pd.isna(df[info]) is True where the dataframe is NaN
                 #~pd.isna(df[info]) is True where the dataframe is NOT NaN
 
-               val=notna.iloc[-1][info] #last not NaN value (not latest day!)
-               dif=notna.iloc[-1][info]-notna.iloc[-2][info] # last not NaN value - second last not NaN value
+                val=notna.iloc[-1][info] #last not NaN value (not latest day!)
+                dif=notna.iloc[-1][info]-notna.iloc[-2][info] # last not NaN value - second last not NaN value
 
             if label != "Rt":
                 # Show the values as integers
@@ -118,18 +122,18 @@ for Row in Rows: #Row is every key in dictionary Rows
             else:
                 ci.metric(label=label,value= round(val,2), delta = str(round(dif,2)), delta_color = 'inverse')
 
+                
 
-# ----------------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------- page row 1 ---------------------------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------------------------------
-row_spacer_start, R0_, m_,m_global_, CFR_  =st.columns((0.5,1.0,1.0,1.0,1.0)) 
+
+row_spacer_start, R0_, m_,m_global_, CFR_  = st.columns((0.5,1.0,1.0,1.0,1.0)) 
 with row_spacer_start:
     st.markdown("Epidemiological Indicators")
-    R0_.metric(label="Basic Reproduction Number - Ro",value= R0)
-    m_.metric(label="Mortality (Greece)",value= m)
-    m_global_.metric(label="Mortality (Global)",value= m_global)
-    CFR_.metric(label="Case Fatality Rate",value= round(CFR,3))
-
+R0_.metric(label="Basic Reproduction Number - Ro",value= R0)
+m_.metric(label="Mortality (Greece)",value= m)
+m_global_.metric(label="Mortality (Global)",value= m_global)
+CFR_.metric(label="Case Fatality Rate",value= round(CFR,3))
+                
 row_spacer_start, row1, row2, row_spacer_end  = st.columns((0.1, 1.0, 6.4, 0.1))
 
 with row1:
@@ -137,104 +141,73 @@ with row1:
     plot_value = st.selectbox ("Variable", list(value_labels.keys()), key = 'value_key') #take all the keys from value_labels dictionary
     plot_value2 = st.selectbox ("Second Variable", [None]+list(value_labels.keys()), key = 'value_key')
     smooth = st.checkbox("Add smooth curve")
-
-
+   
+    
 with row2:    
     sec= not (plot_value2 is None) #True or False if there is a second plot
-
+    
     fig = make_subplots(specs=[[{"secondary_y": sec}]]) #plotly function, define fig which will be show at user
-
+    
     x1=df.index #abbreviation for dates
     y1=df[value_labels[plot_value]] #abbreviation for ploting values, translate from shown names to column names (from value_labels dictionary)
-
-    #fig1= px.bar(df,x = x1, y=value_labels[plot_value])#,log_y=log)
-
+    
+    #fig1= px.bar(df,x = x1, y=value_labels[plot_value])
+    
     fig1= px.bar(df,x = x1, y=y1) #bar plot named as fig1
-
-fig.add_traces(fig1.data) #add to the fig (what is going to be show to the user) the fig1
-
-
+    
+    fig.add_traces(fig1.data) #add to the fig (what is going to be show to the user) the fig1
+    
+    
     #fig.layout.yaxis.title=plot_value #add label
-
-if smooth:
+    
+    if smooth:
         # Create temprary rolling average dataframe
-  ys1= df.rolling("7D").mean()[value_labels[plot_value]]
-  figs=px.line(x = x1, y=ys1)
-fig.add_traces(figs.data)   #add figs to the fig (what we will show at the end)     
+        ys1= df.rolling("7D").mean()[value_labels[plot_value]]
+        figs=px.line(x = x1, y=ys1)
+        fig.add_traces(figs.data)   #add figs to the fig (what we will show at the end)     
+        
+    if sec:
+        x2=df.index
+        y2=df[value_labels[plot_value2]]
+        figsec=px.line(x = x2, y=y2)
+        figsec.update_traces(yaxis="y2")
+        
+        fig.add_traces(figsec.data) #add figsec to the fig (what we will show at the end) 
+        fig.layout.yaxis2.title=plot_value2
 
-if sec:
-   x2=df.index
-   y2=df[value_labels[plot_value2]]
-figsec=px.line(x = x2, y=y2)#,log_y=log)
-figsec.update_traces(yaxis="y2")
-
-fig.add_traces(figsec.data) #add figsec to the fig (what we will show at the end) 
-fig.layout.yaxis2.title=plot_value2
-
-fig.update_layout(title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=plot_value, xaxis_title=None)
-st.plotly_chart(fig, use_container_width=True) 
+    fig.update_layout(title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=plot_value, xaxis_title=None)
+    st.plotly_chart(fig, use_container_width=True) 
 
 
-# ----------------------------------------------------------------------------------------------------------------------------------------
+
 # ------------------------------------------------------- page row 2 ---------------------------------------------------------------------  
-# ----------------------------------------------------------------------------------------------------------------------------------------
-row_spacer_start_row2, dependent_variable  = st.columns((0.1,4.0)) 
 
+row_spacer_start_row2, dependent_variable  = st.columns((0.1,4.0)) 
+                
 row_spacer_start, row1, row2, row_spacer_end  = st.columns((0.1, 1.0, 6.4, 0.1))
 
 with row1:
     #add here everything you want in first column
     #plot_value = st.selectbox ("Linear regression", list(value_labels.keys()), key = 'value_key') #take all the keys from value_labels dictionary
-    st.subheader("Linear Regression")
+    st.subheader("Near Future Projection")
 
 if st.checkbox("Display dataset", False):
     st.subheader("COVID Greece dataset")
     st.write(df) 
-
+    
 # ----------------------------------------- linear regression -----------------------------------------#
-# input arrays
-#x = np.array(df[['id']])
-#y = np.array(df['new_cases'])
-
-# Create an instance of liner regression
-#lm = LinearRegression()
-#model = lm.fit(x, y)
-
-#y_pred = lm.predict(x)
-#r_squared = lm.r2_score(x, y)
-
-#figline = sns.regplot(x='dates', y='new_cases', data=df)
-#st.pyplot(figline)
-
-### Accuracy of the model
-#"""
-#R2 = r2_score(Y_val, Y_pred_val)
-#st.write(f'R2 value: {R2:.2f}')
-
-# Plot
-#with row2: 
-#fig, ax = plt.subplots(figsize=(5, 3))
-#ax.scatter(x=df['id'], y=df['new_cases'])
-#sns.regplot(x='date', y='new_cases', data=df)
-#plt.xlabel('date')
-#plt.ylabel('new_cases')
-#st.pyplot(fig)
-
-#fig_LR = px.scatter(df, x=df["date"], y=df["new_cases"], trendline='ols', title='Ordinary Least Squares Regression') 
-
-
-# -----------------------------------------------------------------------------------------------------#
 
 with row2:      
     #sec= not (plot_value2 is None) #True or False if there is a second plot
-
+        
     x_LR = df.index #abbreviation for dates
     y_LR = df["new_cases"] #abbreviation for ploting values, translate from shown names to column names (from value_labels dictionary)
-
+    
     fig = px.scatter(df, x=x_LR, y=y_LR, trendline="ols")
     fig.show()
-
+    
     #fig1= px.bar(df,x = x1, y=value_labels[plot_value])#,log_y=log)
     #fig_LR= px.bar(df,x = x_LR, y=y_LR) #bar plot named as fig1
+    
     fig.update_layout(title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=plot_value, xaxis_title=None)
     st.plotly_chart(fig, use_container_width=True) 
